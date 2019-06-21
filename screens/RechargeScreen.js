5e3eceb05c77fb2d90508625fcde48c08fb44509
daya-gupta <dayanandorgupta@gmail.com>
2019-06-21 14:57:13 +0530

    implementing camera and gallery access

--- screens/RechargeScreen.js
+++ screens/RechargeScreen.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
-import { Platform,StyleSheet, Text, View, TextInput, Button, TouchableHighlight, Alert, Modal,PermissionsAndroid} from 'react-native';
+import { Platform,StyleSheet, Text, View, TextInput, Button, TouchableHighlight,
+    Alert, Modal, PermissionsAndroid, TouchableOpacity, ScrollView, Image } from 'react-native';
 import Contacts from 'react-native-contacts';
 import { HeaderBackButton } from 'react-navigation';
+import { RNCamera } from 'react-native-camera';
+import CameraRoll from "@react-native-community/cameraroll";
 
 export default class RechargeScreen extends React.Component {
     static navigationOptions = ({ navigation }) => {
@@ -15,7 +18,13 @@ export default class RechargeScreen extends React.Component {
           )
         };
       }
-    state = { textInput: '', showModal: false, contacts: [] };
+    state = {
+        textInput: '',
+        showModal: false,
+        contacts: [],
+        photos: [],
+        galleryLoaded: false
+    };
     async _requestContactPermissionAndroid() {
         try {
             const granted = await PermissionsAndroid.request(
@@ -127,6 +136,41 @@ export default class RechargeScreen extends React.Component {
             </Modal>
         );
     }
+    takePicture = () => {
+        if (this.camera) {
+          const options = { quality: 0.5, base64: true };
+          console.log(this.camera.current);
+          this.camera.takePictureAsync()
+            .then(data => {
+                console.log(data);
+                CameraRoll.saveToCameraRoll(data.uri)
+                    .then(() => Alert.alert('Success', 'Photo added to camera roll!'))
+                    .catch(err => Alert.alert('err:', JSON.stringify(err)));
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    };
+
+    _loadGalleryImages = () => {
+        if (this.state.galleryLoaded) {
+            this.setState({
+                photos: [], galleryLoaded: false
+            })
+            return;
+        }
+        CameraRoll.getPhotos({
+            first: 20,
+            assetType: 'Photos',
+        })
+        .then(r => {
+            this.setState({ photos: r.edges, galleryLoaded: true });
+        })
+        .catch((err) => {
+            //Error Loading Images
+        });
+    };
     render() {
         return (
             <View style={styles.container}>
@@ -135,6 +179,59 @@ export default class RechargeScreen extends React.Component {
                 </Text>
                 {this.renderContactsForm()}
                 {this.renderContactsModal()}
+
+                <View className="camera-section" style={styles.camContainer}>
+                    <Text>Welcome to React Native!</Text>
+                    <Text>To get started, edit App.js</Text>
+                    {/* <Text>{JSON.stringify(RNCamera.Constants.aspect)}</Text> */}
+                    
+                    <RNCamera
+                        ref={ref => {
+                            this.camera = ref;
+                        }}
+                        style={styles.preview}
+                        type={RNCamera.Constants.Type.back}
+                        flashMode={RNCamera.Constants.FlashMode.on}
+                        androidCameraPermissionOptions={{
+                            title: 'Permission to use camera',
+                            message: 'We need your permission to use your camera',
+                            buttonPositive: 'Ok',
+                            buttonNegative: 'Cancel',
+                        }}
+                        androidRecordAudioPermissionOptions={{
+                            title: 'Permission to use audio recording',
+                            message: 'We need your permission to use your audio',
+                            buttonPositive: 'Ok',
+                            buttonNegative: 'Cancel',
+                        }}
+                        onGoogleVisionBarcodesDetected={({ barcodes }) => {
+                            console.log(barcodes);
+                        }}
+                    />
+                        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                        <TouchableOpacity onPress={() => this.takePicture()} style={styles.capture}>
+                            <Text style={{ fontSize: 14 }}> SNAP </Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View>
+                        <Button title={this.state.galleryLoaded ? 'Unload gallery' : 'Load gallery'} onPress={this._loadGalleryImages} />
+                        <ScrollView>
+                        {this.state.photos.map((p, i) => {
+                        return (
+                            <Image
+                            key={i}
+                            style={{
+                                width: 300,
+                                height: 100,
+                            }}
+                            source={{ uri: p.node.image.uri }}
+                            />
+                        );
+                        })}
+                        </ScrollView>
+                    </View>
+                </View>
             </View>
         );
     }
@@ -167,7 +264,27 @@ const styles = StyleSheet.create({
         position: 'absolute',
         right: 0,
         top: 10,
-    }
+    },
+    camContainer: {
+        flex: 1,
+        // flexDirection: 'row',
+        backgroundColor: 'black',
+        // width: 100
+    },
+    preview: {
+        // flex: 1,
+        // justifyContent: 'flex-end',
+        // alignItems: 'center',
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
+    },
 });
 
 
